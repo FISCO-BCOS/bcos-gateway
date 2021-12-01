ed5d27c31c4674b34e8c35dab6206a670e2a29c6
XingQiang Bai <bxq2011hust@qq.com>
2021-12-01 17:50:29 +0800

    update framework (#46)

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,19 +1,30 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/d74ae25cbc0b4c84fcf97664e4ea8f751a530df4.tar.gz
-    SHA1 d8476b5bceb8fb169e58a389e4d0688a01e70821
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/2a3beb81f02fc4c5cdd39c333ae5021b12a6d708.tar.gz
+	SHA1 9bd7b4be1511ce6321f8d4b26c1dff8af11e3729
 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
-hunter_config(bcos-crypto
-	VERSION 3.0.0-local
+# hunter_config(wedpr-crypto VERSION 1.2.0-995589bd
+# 	URL https://${URL_BASE}/WeBankBlockchain/WeDPR-Lab-Crypto/archive/995589bd768d6d70c200c29541fca6714199f8b0.tar.gz
+# 	SHA1 f33b763c1b023965a9d199e288bf503a30293b8c
+# 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=OFF HUNTER_PACKAGE_LOG_INSTALL=ON
+# )
+
+hunter_config(tarscpp VERSION 3.0.3-7299ad23
+	URL https://${URL_BASE}/bxq2011hust/TarsCpp/archive/7299ad23830b50ca6284e11bb0374f2670f23cdf.tar.gz
+	SHA1 9667c0d775bbbc6400a47034bee86003888db978
+)
+
+hunter_config(bcos-crypto VERSION 3.0.1-local
 	URL https://${URL_BASE}/FISCO-BCOS/bcos-crypto/archive/25c8edb7d5cbadb514bbce9733573c8ffdb3600d.tar.gz
 	SHA1 4a1649e7095f5db58a5ae0671b2278bcccc25f1d
+	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=OFF HUNTER_PACKAGE_LOG_INSTALL=ON
 )
 
 hunter_config(bcos-tars-protocol
     VERSION 3.0.0-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-tars-protocol/archive/502c17b20558f4fd3809135622ca68c71b8ff16d.tar.gz
-    SHA1 7f94aa82374b3dbbeaa10af056fcea14bd72720a
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-tars-protocol/archive/6bc78eee8426acca811d154ca87b213cf8ba859a.tar.gz
+    SHA1 38ca3f1606583cd25ec1a2ce6cb714a772f0f3ec
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON URL_BASE=${URL_BASE}
 )
 
